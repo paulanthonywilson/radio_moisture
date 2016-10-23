24ee34134a2e101a2d04e6a84113c239d578b6d6
Paul Wilson <paulanthonywilson@gmail.com>
2016-10-23 21:54:13 +0100

    read and Serial print radio value

---
+++ arduino/radio_receiver/radio_receiver.ino
@@ -0,0 +1,29 @@
+#include <VirtualWire.h>
+
+byte message[VW_MAX_MESSAGE_LEN]; // a buffer to store the incoming messages
+char messageChr[VW_MAX_MESSAGE_LEN + 1];
+
+
+void setup() {
+  Serial.begin(115200);
+ 
+  vw_setup(2000);
+  vw_rx_start();
+
+}
+
+void loop() {
+  readRf();
+}
+
+void readRf(){
+  byte messageLength = VW_MAX_MESSAGE_LEN; // the size of the message
+  if (vw_get_message(message, &messageLength)) {
+    for (int i = 0; i < messageLength; i++) {
+      messageChr[i] = message[i];
+    }
+    messageChr[messageLength] = 0;
+    Serial.println(messageChr);
+  } 
+}
+
